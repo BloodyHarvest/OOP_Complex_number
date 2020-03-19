049e025e32b7ec5e239ce83894cc8a3fd3ab9c83
ihor nesterov <igornes190@gmail.com>
2020-03-19 03:50:57 +0200

    g

--- OOP_Complex_number/Complex_number.h
+++ OOP_Complex_number/Complex_number.h
@@ -335,7 +335,7 @@ void Complex<T1>::print_complex_number()
 	case EXP_FORM:
 		indication = getExpForm();
 		cout << "Exp form:";
-		cout << "z=" << indication[0] << "*e^i*" << indication[1] << "ï" << "\n";
+		cout << "z=" << indication[0] << "*e^i*" << indication[1] << "pi" << "\n";
 		break;
 	}
 }
