14bed92df4602d4e1b3a9fb2b4aa9efcd73dd9b5
ihor nesterov <igornes190@gmail.com>
2020-05-31 14:39:01 +0300

    Lab_1 Completed

--- OOP_Complex_number/Source.cpp
+++ OOP_Complex_number/Source.cpp
@@ -4,14 +4,14 @@
 void print_menu(void)
 {
     cout << "\n -----------------------------------------------\n";
-    cout << "|0.  Exit from Application                      |\n";
-    cout << "|1.  Switch form.                               |\n";
-    cout << "|2.  Write to number.                           |\n";
-    cout << "|3.  Display number.                            |\n";
-    cout << "|4.  X + Y.                                     |\n";
-    cout << "|5.  X - Y.                                     |\n";
-    cout << "|6.  X * Y.                                     |\n";
-    cout << "|7.  X / Y.                                     |\n";
-    cout << "|8.  Raise to power/root                        |\n";
+    cout << "|0. Exit from Application                       |\n";
+    cout << "|1. Switch form.                                |\n";
+    cout << "|2. Write to number.                            |\n";
+    cout << "|3. Display number.                             |\n";
+    cout << "|4. X + Y.                                      |\n";
+    cout << "|5. X - Y.                                      |\n";
+    cout << "|6. X * Y.                                      |\n";
+    cout << "|7. X / Y.                                      |\n";
+    cout << "|8. Raise to power/root                         |\n";
     cout << " -----------------------------------------------\n";
 }
